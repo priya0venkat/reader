b0497b8a4f36a1edb300ab4b52551b5930826641
Abhishek Verma <vermaabhishekp@gmail.com>
2025-12-17 23:32:27 -0800

    Add Phonics Reader to games hub

--- reader/frontend/vite.config.js
+++ reader/frontend/vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/words': 'http://localhost:8000',
-      '/story': 'http://localhost:8000',
-      '/feedback': 'http://localhost:8000',
-    }
-  }
+  base: '/reader/',
 })
