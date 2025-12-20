d08c275ac0bbe822ee4e80f6753a7a320ff4d0d1
Abhishek Verma <vermaabhishekp@gmail.com>
2025-12-19 23:01:23 -0800

    Use GCS hosted OGG file for washing machine sound

--- washing-machine/src/utils/audio.js
+++ washing-machine/src/utils/audio.js
@@ -1,7 +1,8 @@
-import washingMachineSound from '../assets/washing-machine.ogg';
+// Sound file hosted on GCS to avoid git binary issues
+const washingMachineSoundUrl = 'https://storage.googleapis.com/purejax-data-1234/assets/washing-machine.ogg';
 
 export function playWashingMachineSound() {
-    const audio = new Audio(washingMachineSound);
+    const audio = new Audio(washingMachineSoundUrl);
     audio.play().catch(error => {
         console.error("Audio playback failed:", error);
     });
